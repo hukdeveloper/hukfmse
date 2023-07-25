0af0c5415cb27b2ae744b4ed71497366791f3f25
Haris Umar <haris29268@gmail.com>
2023-07-25 22:05:50 +0500

    Initial Commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "../page.module.css";
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <div className={styles.header}>
+      <Link href="/">
+        <div className={styles.header_name}>HUK</div>
+      </Link>
+
+      <div className={styles.header_menu}>
+        <Link href="/articles">Articles</Link>
+        <Link href="/project">Project</Link>
+        <Link href="/about">About</Link>
+      </div>
+    </div>
+  );
+}
